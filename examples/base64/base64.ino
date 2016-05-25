2fef9f7d39418ab5578f909b122d1bc70cc2c104
Charon351 <karel.blavka@gmail.com>
2016-05-25 23:16:20 +0200

    update example

--- examples/base64/base64.ino
+++ examples/base64/base64.ino
@@ -4,7 +4,10 @@
  Base64 Encode/Decode example
  
  Encodes the text "Hello world" to "SGVsbG8gd29ybGQA" and decodes "Zm9vYmFy" to "foobar"
+ Encodes the text "Man" to "TWFu" and decodes "TWFu" to {77,97,110}  or decodes "TWFu" to "Man" 
 
+ {77,97,110} is Man in ASCII
+ 
  Created 29 April 2015
  by Nathan Friedly - http://nfriedly.com/
  
@@ -39,8 +42,6 @@ void setup()
   
   Serial.println(encoded);
   
-  
-  
   // decoding
   char input2[] = "Zm9vYmFy";
   int input2Len = sizeof(input2);
@@ -51,6 +52,47 @@ void setup()
   base64_decode(decoded, input2, input2Len);
   
   Serial.print(input2); Serial.print(" = "); Serial.println(decoded);
+
+  Serial.println("======= base64_encode ========");
+
+  char in[] = "Man";
+  char out[10];
+
+  base64_encode(out, in, 3);
+  Serial.println(out);
+  
+  uint8_t in2[] = {77,97,110};//Man in ASCII
+  base64_encode(out, in2, 3);
+  Serial.println(out);
+
+  byte in3[] = {77,97,110};
+  base64_encode(out, in3, 3);
+  Serial.println(out);
+  
+
+  Serial.println("======= base64_decode ========");
+
+  char in4[] = "TWFu";
+  uint8_t out2[10];
+  
+  base64_decode(out2, in4,4);
+  for(byte i=0; i < 3; i++){
+    Serial.print(out2[i]);  
+  }
+  Serial.println();
+
+  byte out3[10];
+  
+  base64_decode(out3, in4,4);
+  for(byte i=0; i < 3; i++){
+    Serial.print(out3[i]);  
+  }
+  Serial.println();
+
+  char out4[10];
+  base64_decode(out4, in4,4);
+  Serial.println(out4);
+  
 }
 
 
