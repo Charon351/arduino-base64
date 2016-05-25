d5c3d250e6ef805f328a00b6256d1903015073da
Charon351 <karel.blavka@gmail.com>
2016-05-25 23:16:07 +0200

    encode  input as generic <byte, char, uint8_t> decode output as generic <byte, char, uint8_t>

--- Base64.h
+++ Base64.h
@@ -2,6 +2,8 @@
  * Copyright (c) 2013 Adam Rudd.
  * See LICENSE for more information
  */
+#include <avr/pgmspace.h>
+
 #ifndef _BASE64_H
 #define _BASE64_H
 
@@ -26,7 +28,53 @@ extern const char b64_alphabet[];
  * 			2. input must not be null
  * 			3. inputLen must be greater than or equal to 0
  */
-int base64_encode(char *output, char *input, int inputLen);
+template<typename T>int base64_encode(char *output, T *input, size_t inputLen) 
+{
+  int i = 0, j = 0;
+  int encLen = 0;
+  unsigned char a3[3];
+  unsigned char a4[4];
+
+  while(inputLen--) {
+    a3[i++] = *(input++);
+    if(i == 3) {
+        a4[0] = (a3[0] & 0xfc) >> 2;
+        a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
+        a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
+        a4[3] = (a3[2] & 0x3f);
+
+      for(i = 0; i < 4; i++) {
+        output[encLen++] = pgm_read_byte(&b64_alphabet[a4[i]]);
+      }
+
+      i = 0;
+    }
+  }
+
+  if(i) {
+    for(j = i; j < 3; j++) {
+      a3[j] = '\0';
+    }
+    
+    a4[0] = (a3[0] & 0xfc) >> 2;
+	a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
+	a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
+	a4[3] = (a3[2] & 0x3f);
+    
+    for(j = 0; j < i + 1; j++) {
+      output[encLen++] = pgm_read_byte(&b64_alphabet[a4[j]]);
+    }
+
+    while((i++ < 3)) {
+      output[encLen++] = '=';
+    }
+  }
+  output[encLen] = '\0';
+  return encLen;
+
+}
+
+unsigned char b64_lookup(char c);
 
 /* base64_decode:
  * 		Description:
@@ -44,7 +92,57 @@ int base64_encode(char *output, char *input, int inputLen);
  * 			2. input must not be null
  * 			3. inputLen must be greater than or equal to 0
  */
-int base64_decode(char *output, char *input, int inputLen);
+template<typename T> int base64_decode(T *output, char *input, int inputLen)
+{
+	int i = 0, j = 0;
+	int decLen = 0;
+	unsigned char a3[3];
+	unsigned char a4[4];
+
+
+	while (inputLen--) {
+		if(*input == '=') {
+			break;
+		}
+
+		a4[i++] = *(input++);
+		if (i == 4) {
+			for (i = 0; i <4; i++) {
+				a4[i] = b64_lookup(a4[i]);
+			}
+
+			a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
+			a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
+			a3[2] = ((a4[2] & 0x3) << 6) + a4[3];
+
+			for (i = 0; i < 3; i++) {
+                output[decLen++] = (T)a3[i];
+			}
+			i = 0;
+		}
+	}
+
+	if (i) {
+		for (j = i; j < 4; j++) {
+			a4[j] = '\0';
+		}
+
+		for (j = 0; j <4; j++) {
+			a4[j] = b64_lookup(a4[j]);
+		}
+
+		a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
+		a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
+		a3[2] = ((a4[2] & 0x3) << 6) + a4[3];
+
+		for (j = 0; j < i - 1; j++) {
+            output[decLen++] = (T)a3[j];
+		}
+	}
+	output[decLen] = '\0';
+	return decLen;
+}
+
 
 /* base64_enc_len:
  * 		Description:
